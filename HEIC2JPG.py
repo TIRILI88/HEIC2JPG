17ed4498607eed01404ca71ad463e961070e1cfc
TIRILI88 <59590490+TIRILI88@users.noreply.github.com>
2020-05-18 22:26:11 -0400

    Version 1.1

--- HEIC2JPG.py
+++ HEIC2JPG.py
@@ -1,8 +1,12 @@
 import os
 
-dir =input('Hier das Verzechnis mit den Bilder eingeben und Enter druecken: ')
+dir = input('Hier das Verzechnis mit den Bilder eingeben und Enter druecken: ')
+counter = 0
 for i in os.listdir(dir):
     files = os.path.join(dir,i)
     split= os.path.splitext(files)
     if split[1]=='.HEIC':
        os.rename(files,split[0]+'.JPG')
+       counter += 1
+
+print(str(counter) + " Dateien erfolgreich umgewandelt.")
